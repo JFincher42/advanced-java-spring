b938a7eeeb835f3b97db0a5fa9ff0dac11c56adf
Jon Fincher <jfincher42@gmail.com>
2023-03-28 15:10:48 -0500

    Lifecycle callbacks

--- src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/LifecycleCallbackDemo.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/LifecycleCallbackDemo.java
@@ -1,5 +1,6 @@
 package platform.codingnomads.co.springdata.example.dml.lifecyclecallback;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class LifecycleCallbackDemo {
 
+    @Autowired
+    PrintEntityRepository printEntityRepository;
     public static void main(String[] args) {
         SpringApplication.run(LifecycleCallbackDemo.class);
     }
@@ -16,6 +19,13 @@ public class LifecycleCallbackDemo {
     public CommandLineRunner runStuff(PrintEntityRepository printEntityRepository) {
         return (args) -> {
             // put your logic here
+            PrintEntity printEntity = new PrintEntity();
+
+            printEntity = printEntityRepository.save(printEntity);
+
+            printEntity.setId(printEntity.getId() + 2);
+
+            printEntity = printEntityRepository.save(printEntity);
 
         };
     }
--- src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/PrintEntity.java
+++ src/main/java/platform/codingnomads/co/springdata/example/dml/lifecyclecallback/PrintEntity.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -20,5 +17,19 @@ public class PrintEntity {
     private Long id;
 
     // write your methods here
+    @PrePersist
+    @PreUpdate
+    private void printPrePersist(){
+        System.out.println("Updating or Saving PrintEntity " + id + " now...");
+    }
 
+    @PostPersist
+    private void printPostPersist(){
+        System.out.println("PrintEntity " + this.id + " persisted before this");
+    }
+
+    @PostUpdate
+    private void printPostUpdate(){
+        System.out.println("Just updated PrintEntity " + id + "...");
+    }
 }
